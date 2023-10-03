ed6b22eaf93a2525fb10a3d51bb6d251497aeadf
Erfan Baghalpour <erfanbaghalpour@gmail.com>
2023-10-03 19:45:52 +0330

    ch5

--- bookStore_project/settings.py
+++ bookStore_project/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'accounts.apps.AccountsConfig'
+    'accounts.apps.AccountsConfig',
+    'pages.apps.PagesConfig'
 ]
 
 AUTH_USER_MODEL = "accounts.CustomUser"
