63c777f26c9fb5ea620e37f5bfddc7458ebc6f85
Erfan Baghalpour <erfanbaghalpour@gmail.com>
2023-10-07 18:53:41 +0330

    ch13

--- bookStore_project/settings.py
+++ bookStore_project/settings.py
@@ -149,6 +149,9 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
+MEDIA_URL = "/media/"  # new
+MEDIA_ROOT = BASE_DIR / "media"  # new
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- books/models.py
+++ books/models.py
@@ -1,5 +1,5 @@
 import uuid  # new
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model  # new
 from django.db import models
 from django.urls import reverse
 
@@ -12,6 +12,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    cover = models.ImageField(upload_to="covers/", blank=True)  # new
 
     def __str__(self):
         return self.title
@@ -20,16 +21,16 @@ class Book(models.Model):
         return reverse("book_detail", args=[str(self.id)])
 
 
-class Review(models.Model):
+class Review(models.Model):  # new
     book = models.ForeignKey(
         Book,
         on_delete=models.CASCADE,
-        related_name="reviews"
+        related_name="reviews",
     )
     review = models.CharField(max_length=255)
     author = models.ForeignKey(
         get_user_model(),
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
     )
 
     def __str__(self):
